ec407fec276f93aa6f3451e7eebd1b18c9a42d16
nevor1530 <nevor1530@163.com>
2014-05-05 18:45:12 +0800

    change subject side menu

--- protected/modules/admin/views/common/subject_side_nav.php
+++ protected/modules/admin/views/common/subject_side_nav.php
@@ -1,6 +1,5 @@
 <?php
 $this->sideNav=array(
 	array('label'=>'考点树设置', 'url'=>array('/admin/examPoint/index', 'subject_id'=>$subjectModel->subject_id)),
-	array('label'=>'推荐真题', 'url'=>array('index')),
-	array('label'=>'真题试卷分类', 'url'=>array('index')),
+	array('label'=>'试卷管理', 'url'=>array('/admin/examPaper/index', 'subject_id'=>$subjectModel->subject_id)),
 );
